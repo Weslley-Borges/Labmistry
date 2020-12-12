790f17fe263a2913b0b3ef39c02fa0bab500f264
Weslley Borges <60989781+Weslley-Borges@users.noreply.github.com>
2020-12-12 07:25:13 -0300

    feat(web): init chat landing page I'm creating a landing page for the Labmistry's chat even though I still prefer to redirect the user to a chat on whatsapp

---
+++ web/src/pages/chat/landingChat.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import '../../assets/styles/pages/chat/landingChat.scss'
+
+/* 
+	src/pages/classroom/landing.tsx, 11/18/2020
+	Author: Weslley Borges dos Santos
+	Este arquivo é o frontend da landing page dos chats
+*/
+
+export default function Landing() {
+	return (
+		<div id="page-landingChat">
+			
+		</div>
+)}
