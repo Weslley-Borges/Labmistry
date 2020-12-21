777db3cbd4a4aa634b9ebc8c1c0937fcaa9de3a7
Weslley Borges <60989781+Weslley-Borges@users.noreply.github.com>
2020-12-20 22:33:59 -0300

    feat(web[Home]): Adding Sidebar Eu estou adicionando uma sidebar à pedido de Linecker.

--- web/src/pages/user/home.tsx
+++ web/src/pages/user/home.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { RiMenu4Line, RiHome2Line } from 'react-icons/ri'
+import { AiOutlineLogout, AiOutlineSetting, AiOutlineSearch } from 'react-icons/ai'
+import { IoGameControllerOutline } from 'react-icons/io5'
 import { Link } from 'react-router-dom'
 
-import '../assets/styles/pages/user/home.scss'
+import '../../assets/styles/pages/user/home.scss'
+
+import LogoImg from '../../assets/images/logo.svg'
+import students from '../../assets/images/Students.svg'
 
 /* 
 	src/pages/user/home.tsx, 12/18/2020
@@ -12,6 +18,39 @@ import '../assets/styles/pages/user/home.scss'
 export default function Home() {
 	return (
 		<div id="page-home">
+			<div className="page-home-content">
+				<input type="checkbox" id="menu"/>
 
+				<nav>
+				<img src={LogoImg} alt="Labmistry"/>
+					<ul>
+						<li><a href="#">Logout</a></li>
+					</ul>
+
+					<label htmlFor="menu" className="menu-bar">
+						<RiMenu4Line size="3rem" color="#fff"/>
+					</label>
+
+				</nav>
+
+				<div className="side-menu">
+					<main>
+						<img src={students} alt=""/>
+						<br/>
+						<h2>Um estudante</h2>
+					</main>
+					<br/>
+
+					<Link to="/" className="menu_option"> <RiHome2Line size="3rem" color="#fff" className= "option_img"/><span>Home</span></Link>
+					<Link to="/" className="menu_option"> <AiOutlineSearch size="3rem" color="#fff" className= "option_img"/><span>Search</span></Link>
+					<Link to="/" className="menu_option"> <IoGameControllerOutline size="3rem" color="#fff" className= "option_img"/><span>Play</span></Link>
+					<Link to="/" className="menu_option"> <AiOutlineSetting size="3rem" color="#fff" className= "option_img"/><span>Settings</span></Link>
+
+					<Link to="/" className="menu_option"> <AiOutlineLogout size="3rem" color="#fff" className= "option_img logout"/><span className="logout">LogOut</span></Link>
+
+
+				</div>
+			</div>
+			
 		</div>
 )}
--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import Landing from './pages/landing'
-import LandingChat from './pages/chat/landingChat'
-import NewChat from './pages/chat/newChat'
 import Login from './pages/login'
 import ChooseRegister from './pages/chooseRegister'
 import RegisterStudent from './pages/registerStudent'
@@ -12,6 +10,11 @@ import TeacherProfile from './pages/teacherProfile'
 
 import PeriodicTable from './pages/periodicTable'
 
+import Home from './pages/user/home'
+
+
+import LandingChat from './pages/chat/landingChat'
+import NewChat from './pages/chat/newChat'
 
 
 export default function Routes() {
@@ -24,6 +27,10 @@ export default function Routes() {
 				<Route path="/create/student" exact component={RegisterStudent}/>
 				<Route path="/create/teacher" exact component={RegisterTeacher}/>
 				<Route path="/user/teacher" exact component={TeacherProfile}/>
+
+				{/*Páginas principais (vão precisar da autenticação do usuário)*/}
+				<Route path="/home" exact component={Home}/>
+
 
 				<Route path="/periodc" exact component={PeriodicTable}/>
 				<Route path="/chat" exact component={LandingChat}/>
